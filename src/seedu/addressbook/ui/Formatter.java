5e1f2005901e698444eb8c3af0de815d486ef75a
wangzexin <wang.zexin@dhs.sg>
2017-02-01 12:04:17 +0800

    [T3A5] Remove redundant importing of packages

--- src/seedu/addressbook/ui/Formatter.java
+++ src/seedu/addressbook/ui/Formatter.java
@@ -1,17 +1,5 @@
 package seedu.addressbook.ui;
 
-import static seedu.addressbook.common.Messages.*;
-
-import seedu.addressbook.commands.CommandResult;
-import seedu.addressbook.data.person.ReadOnlyPerson;
-
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
-
 public class Formatter {
 	/** A decorative string added to the beginning or end of lines printed by AddressBook */
     private String _decorator;
