ea0d227f744bc7223a353390c4f16591ab908b1b
wangzexin <wang.zexin@dhs.sg>
2017-02-04 16:39:19 +0800

    [T4A1] No change at all. To distinguish between Whole and Parts, Container and Contained, check call hierachy, the Parts should be only created by their Wholes.

--- src/seedu/addressbook/data/tag/Tag.java
+++ src/seedu/addressbook/data/tag/Tag.java
@@ -6,7 +6,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Tag in the address book.
  * Guarantees: immutable; name is valid as declared in {@link #isValidTagName(String)}
  */
-public class Tag {
+public class Tag { 
 
     public static final String MESSAGE_TAG_CONSTRAINTS = "Tags names should be alphanumeric";
     public static final String TAG_VALIDATION_REGEX = "\\p{Alnum}+";
