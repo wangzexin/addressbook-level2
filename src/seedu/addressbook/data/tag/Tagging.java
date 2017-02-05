23d74d25d4a810dbe078a499547382aa47ad2d35
wangzexin <wang.zexin@dhs.sg>
2017-02-05 17:28:17 +0800

    [T4A5] Store totalTaggings as class-level variable of Tagging class

--- src/seedu/addressbook/data/tag/Tagging.java
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -1,5 +1,7 @@
 package seedu.addressbook.data.tag;
 
+import java.util.ArrayList;
+
 import seedu.addressbook.data.person.Person;
 
 public class Tagging{
@@ -10,9 +12,12 @@ public class Tagging{
 	private Tag _tag;
 	private TaggingType _taggingType;
 	
+	public static ArrayList<Tagging> totalTaggings;
+	
 	public Tagging(Person person, Tag tag, TaggingType taggingType){
 		_person = person;
 		_tag = tag;
 		_taggingType = taggingType;
+		totalTaggings.add(this);
 	}
 }
