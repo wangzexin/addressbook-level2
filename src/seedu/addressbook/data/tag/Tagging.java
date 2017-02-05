c98f928e11e2c84ae94b3553f076e4271046a266
wangzexin <wang.zexin@dhs.sg>
2017-02-05 17:20:37 +0800

    [T4A5] Create associate class Tagging, use enum for add or delete

---
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -0,0 +1,18 @@
+package seedu.addressbook.data.tag;
+
+import seedu.addressbook.data.person.Person;
+
+public class Tagging{
+	// Types of Tagging
+	private enum TaggingType {ADD, DELETE};
+	
+	private Person _person;
+	private Tag _tag;
+	private TaggingType _taggingType;
+	
+	public Tagging(Person person, Tag tag, TaggingType taggingType){
+		_person = person;
+		_tag = tag;
+		_taggingType = taggingType;
+	}
+}
