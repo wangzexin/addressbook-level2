b74ab61d31d63814a92c2bd45773dc77a907a2ec
wangzexin <wang.zexin@dhs.sg>
2017-02-07 15:36:15 +0800

    [T4A7] Refactor to extract methods to improve SLAP

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -16,6 +16,51 @@ public class NameTest {
 	@Test
 	public void testIsSimilar() throws IllegalValueException{
 
+		testSameFullName();
+		
+		testSupersetSubsetNames();
+		
+		testDifferentCasingNames();
+		
+		testDifferentCasingAndSupersetSubsetNames();
+		
+		testNullName();
+	}
+
+	private void testNullName() throws IllegalValueException {
+		Name normalName = new Name("Super Man");
+		Name nullName = null;
+		assertFalse(normalName.isSimilar(nullName));
+	}
+
+	private void testDifferentCasingAndSupersetSubsetNames() throws IllegalValueException {
+		Name differentCaseSupersetName = new Name("Jay CHOU Zhou");
+		Name differentCaseSubsetName1 = new Name("jAY cHOU");
+		Name differentCaseSubsetName2 = new Name("Jay Chou");
+		
+		assertTrue(differentCaseSubsetName1.isSimilar(differentCaseSupersetName));
+		assertTrue(differentCaseSupersetName.isSimilar(differentCaseSubsetName1));
+		assertTrue(differentCaseSubsetName2.isSimilar(differentCaseSupersetName));
+		assertTrue(differentCaseSupersetName.isSimilar(differentCaseSubsetName2));
+	}
+
+	private void testDifferentCasingNames() throws IllegalValueException {
+		Name differentCaseName1 = new Name("jAY cHOU");
+		Name differentCaseName2 = new Name("Jay Chou");
+
+		assertTrue(differentCaseName1.isSimilar(differentCaseName2));
+		assertTrue(differentCaseName2.isSimilar(differentCaseName1));
+	}
+
+	private void testSupersetSubsetNames() throws IllegalValueException {
+		Name supersetName = new Name("Jay Chou Zhou");
+		Name subsetName = new Name("Jay Chou");
+		
+		assertTrue(subsetName.isSimilar(supersetName));
+		assertTrue(supersetName.isSimilar(subsetName));
+	}
+
+	private void testSameFullName() throws IllegalValueException {
 		Name name1 = new Name("Jane Eyre");
 		Name name2 = new Name("Jay Chou");
 		Name name3 = new Name("Jay Chou");
@@ -24,26 +69,5 @@ public class NameTest {
 		assertTrue(name1.isSimilar(name1));
 
 		assertTrue(name2.isSimilar(name3));
-		
-		Name supersetName = new Name("Jay Chou Zhou");
-		Name subsetName = new Name("Jay Chou");
-		
-		assertTrue(subsetName.isSimilar(supersetName));
-		assertTrue(supersetName.isSimilar(subsetName));
-		
-		Name differentCaseName1 = new Name("jAY cHOU");
-		Name differentCaseName2 = new Name("Jay Chou");
-		Name differentCaseSupersetName = new Name("Jay CHOU Zhou");
-		Name nullName = null;
-
-		assertTrue(differentCaseName1.isSimilar(differentCaseName2));
-		assertTrue(differentCaseName2.isSimilar(differentCaseName1));
-		
-		assertTrue(differentCaseName1.isSimilar(differentCaseSupersetName));
-		assertTrue(differentCaseSupersetName.isSimilar(differentCaseName1));
-		assertTrue(differentCaseName2.isSimilar(differentCaseSupersetName));
-		assertTrue(differentCaseSupersetName.isSimilar(differentCaseName2));
-		
-		assertFalse(differentCaseName1.isSimilar(nullName));
 	}
 }
